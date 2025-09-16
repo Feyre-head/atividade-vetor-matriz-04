0ab5818ec8d5bcb8e6d711f6224c4928fdff1cd0
Inara <inaraoenning@gmail.com>
2025-09-16 13:25:37 -0300

    Refactor Atv1 usando Math

--- src/atv1.java
+++ src/atv1.java
@@ -3,40 +3,34 @@ import java.util.Scanner;
 public class atv1 {
     public static void main(String[] args) {
 
-        //Criar vetor [10]
-        //Entrada 10 numeros
-        //for menor número / maior número
+        // Armazenar entrada 10 num vetor usando for
+        // for / if / usar Math min e máx para mostrar valor maior e menor do array
 
         Scanner sc = new Scanner(System.in);
+        int[] listaVetor = new int[10];
 
-        int[] numberList = new int[10];
+        System.out.println("Vamos Armazenar 10 números em um Array e mostrar o Menor e o Maior valor");
 
-        System.out.println("Informe 10 números para descobrir qual é maior e qual é menor");
-
-        for (int i = 0; i < numberList.length; i++) {
+        for (int i = 0; i < listaVetor.length; i++) {
             System.out.println("Informe o [" + (i + 1) + "] Número: ");
-            numberList[i] = sc.nextInt();
+            listaVetor[i] = sc.nextInt();
         }
 
-        int maiorNum = 0;
-        int menorNum = 0;
+        System.out.println("Números informados:");
+        for (int num : listaVetor) {
+            System.out.println(num);
+        }
+        System.out.println("===================");
 
+        int maiorNum = listaVetor[0];
+        int menorNum = listaVetor[0];
 
-        for (int c = 0; c < numberList.length; c++) {
-            if (numberList[c] > maiorNum) {
-                maiorNum = numberList[c];
-            }
-            if (numberList[c] < menorNum) {
-                menorNum = numberList[c];
-            }
+        for (int num : listaVetor) {
+            maiorNum = Math.max(maiorNum, listaVetor[num]);
+            menorNum = Math.min(menorNum, listaVetor[num]);
         }
 
-        System.out.println("maior número é: " + maiorNum);
-        System.out.println("Menor num: " + menorNum);
-
-
+        System.out.println("Maior número é: " + maiorNum);
+        System.out.println("Menor número é: " + menorNum);
     }
 }
-
-
-// menor =
