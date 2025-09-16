cfda5c386665e191855b29f7d68615a0a46c4e9e
Inara <inaraoenning@gmail.com>
2025-09-16 18:01:15 -0300

    Refactor Atv2

--- src/atv2.java
+++ src/atv2.java
@@ -2,32 +2,33 @@ import java.util.Random;
 
 public class atv2 {
     public static void main(String[] args) {
-        // vetor 15 num aleatórios  Random
-        // mostrar pares e ímpares
-
+        // Preencher 15 numeros Randmo em um vetor
+        // Mostrar pares e ímpares
 
         Random random = new Random();
 
         int tamanho = 15;
-        int[] numList = new int[tamanho];
+        int[] vetorLista = new int[tamanho];
+        int[] par = new int[0];
+        int[] impar = new int[0];
+
+        int qtdPares = 0, qtdImpares = 0;
 
 
-        for (int i = 0; i < tamanho; i++) {
-            if (numList[i] < tamanho) {
-                numList[i] = random.nextInt(tamanho);
-                System.out.println("Números Aleatórios:");
-                System.out.println((1 + i) + "- " + numList[i]);
-            }
+        System.out.println("Números Aleatórios Informados::");
+        for (int i = 0; true; i++) {
+            vetorLista[i] = random.nextInt(20);
+            System.out.println((1 + i) + "- " + vetorLista[i]);
 
-        }
-
-        for (int i = 0; i < tamanho; i++) {
-            if (numList[i] % 2 == 0) {
-                System.out.println("Par: " + numList[i]);
+            if (vetorLista[i] % 2 == 0) {
+                par[qtdPares] = vetorLista[i];
+                qtdPares++;
             } else {
-                System.out.println("Ímpar: " + numList[i]);
+                impar[qtdImpares] = vetorLista[i];
+                qtdImpares++;
             }
         }
 
     }
+
 }
