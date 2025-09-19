e5e557ad34adeb6eed57c0dc459792c580769fe0
Inara <inaraoenning@gmail.com>
2025-09-19 11:47:39 -0300

    Add Comentarios Atv6

--- src/atv6.java
+++ src/atv6.java
@@ -1,24 +1,25 @@
 public class atv6 {
     public static void main(String[] args) {
-        //Criar Matriz 5x5
-        // Preencher diagonal com 1 e o restante 0
-        // onde l e c sao iguais colocar 1
+          // Criar Matriz 5x5
+         // Preencher diagonal com 1 e o restante 0
+        // Índiice onde linha e coluna sao iguais colocar 1
 
         int tamanho = 5;
         int[][] matriz = new int[tamanho][tamanho];
 
-        for (int l = 0 ; l < tamanho ; l++){
-            for (int c = 0 ; c < tamanho; c++){
-                if(l == c){
+        for (int l = 0; l < tamanho; l++) {
+            for (int c = 0; c < tamanho; c++) {
+                if (l == c) {         //Se o índice de L e C forem iguais armazena 1
                     matriz[l][c] = 1;
-                }else {
+                } else {             //Senão armazena 0
                     matriz[l][c] = 0;
                 }
             }
         }
 
-        for (int[] num : matriz){ //pega cada linha (array) da matriz
-            for (int numeros : num) {   //pega o numero de cada linha
+        //For each para printar
+        for (int[] num : matriz) {       // Pega cada linha (array) da matriz
+            for (int numeros : num) {   // Pega o numero de cada linha
                 System.out.print(numeros + "  ");
             }
             System.out.println();
