dd5aad345b1492033ea70aabea657b6d54be9091
Inara <inaraoenning@gmail.com>
2025-09-19 11:44:52 -0300

    Add Comentarios Atv5

--- src/atv5.java
+++ src/atv5.java
@@ -6,6 +6,11 @@ public class atv5 {
         //Criar for para somar linha por linha
         //Criar for para somar coluna por coluna
 
+        //    0  1  2
+        // 0  x  x  x
+        // 1  x  x  x
+        // 2  x  x  x
+
         Scanner sc = new Scanner(System.in);
 
         int tamanho = 3;
@@ -14,30 +19,31 @@ public class atv5 {
         System.out.println("Informe 9 numeros para saber a soma de cada linha e Coluna");
         for (int l = 0; l < tamanho; l++) {
             for (int c = 0; c < tamanho; c++) {
-                System.out.println("Informe numero da linha [" + l + "]  e Coluna [" + c + "]");
+                System.out.println("Informe numero da linha [" + l + "]  e Coluna [" + c + "]"); //Informar número de acordo com o índice da linha e coluna
                 numeros[l][c] = sc.nextInt();
             }
         }
 
-        //mostrar Matriz
+        //Mostrar Matriz
         System.out.println("==== Matriz 3x3 ====");
-        for (int[] num : numeros) { //percorre linha (array)
-            for (int elemento : num) { //percorre numero de cada linha
+        for (int[] num : numeros) {      //Percorre linha (array)
+            for (int elemento : num) {  //Percorre numero de cada linha
                 System.out.print(elemento + "   ");
             }
             System.out.println();
         }
 
-//soma linhas
-        for (int l = 0; l < tamanho; l++) {  // percorre as linhas
-            int somaLinha = 0;
-            for (int c = 0; c < tamanho; c++) {   // percorre as colunas
+        //Soma Linhas
+        for (int l = 0; l < tamanho; l++) {         //Percorre as linhas
+            int somaLinha = 0;                     //Variável para armazenar o valor das linhas
+            for (int c = 0; c < tamanho; c++) {   //Percorre as colunas
                 somaLinha = somaLinha + numeros[l][c];
             }
             System.out.println("Soma da linha " + (l + 1) + " = " + somaLinha);
         }
+
         System.out.println("====================");
-        //soma coluna
+        //Soma Coluna
         for (int l = 0; l < tamanho; l++) {
             int somaColuna = 0;
             for (int c = 0; c < tamanho; c++) {
