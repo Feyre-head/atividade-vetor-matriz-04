b3037102c5584fde9019c99fd2d2c2db955f569b
Inara <inaraoenning@gmail.com>
2025-09-19 08:54:55 -0300

    Add Comentarios Atv2

--- src/atv2.java
+++ src/atv2.java
@@ -2,20 +2,25 @@ import java.util.Random;
 
 public class atv2 {
     public static void main(String[] args) {
-        // Preencher 15 numeros Random
-        // for para pares e for para ímpares
+        // Preencher 15 numeros com Random
+        // Usar um for para pares e outro for para ímpares
         // Mostrar pares e ímpares
 
         Random random = new Random();
 
+        //Opcional, variavel tamanho do vetor
         int tamanho = 15;
+
+        //Cria vetor com tamanho
         int[] numeros = new int[tamanho];
 
+        //Adiciona os números random do vetor e printa ele
         System.out.println("Números Aleatórios Informados:");
         for (int i = 0; i < tamanho; i++) {
-            numeros[i] = random.nextInt(15);
-            System.out.println((1 + i) + "- " + numeros[i]);
+            numeros[i] = random.nextInt(15);  //Adiciona os números random ao vetor
+            System.out.println((1 + i) + "- " + numeros[i]); //Print dos números
 
+            //Verifica números pares
             System.out.println("Números Pares:");
             for (int num : numeros) {
                 if (num % 2 == 0) {
@@ -23,6 +28,7 @@ public class atv2 {
                 }
             }
 
+            //Verifica Números ímpares
             System.out.println("Números Ímpares");
             for (int num : numeros) {
                 if (num % 2 != 0) {
